47567ee95cfb05619bb12e983fe8f3ab45bc5d56
as <>
2021-02-04 06:00:05 +0900

    鳥を動かしてターゲットに向かわせるスクリプト

--- Assets/MyScript/BirdController.cs
+++ Assets/MyScript/BirdController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class BirdController : MonoBehaviour
 {
+    public Transform targetFocus;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetFocus = GameObject.FindGameObjectWithTag("target").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 target = targetFocus.position - this.transform.position;
+        Debug.Log(target.magnitude);
+
+        transform.LookAt(targetFocus.transform);
+        float speed = Random.Range(15f, 30f);
+        transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
